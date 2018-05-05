3484d66bfcd7635f813dd2e44b66e0a754088eca
Your Name <you@example.com>
2018-05-05 10:35:15 +0200

    renamed,portage added

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-      name="dict.cc.py",
+      name="dictccpy",
       description="Unofficial dict.cc command line interface",
       version="2.0.1",
       url="http://github.com/rbaron/dict.cc.py",
       install_requires=["beautifulsoup4"],
       packages=find_packages(),
-      scripts=["scripts/dict.cc.py"]
+      scripts=["scripts/dictcc.py"]
      )
